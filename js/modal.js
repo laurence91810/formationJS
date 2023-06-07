bc76b87bc8db05cff6702607cdd84d75fefc7202
laurence91810 <bertolinlaurence@gmail.com>
2023-06-07 07:54:19 +0100

    création class js

--- js/modal.js
+++ js/modal.js
@@ -1,21 +1,21 @@
-let modalNode = undefined;
+//let modalNode = undefined;
 
-document.addEventListener('DOMContentLoaded',(evt)=>{
-    console.log(evt);
-    modalNode=document.querySelector('#modal');
-    console.log(modalNode);
-    removeModal();
-});
+// document.addEventListener('DOMContentLoaded',(evt)=>{
+//     console.log(evt);
+//     modalNode=document.querySelector('#modal');
+//     console.log(modalNode);
+//     removeModal();
+// });
 
 
 /**
  * fermeture du modal
  */
-const removeModal=()=>{
-    if(undefined!==modalNode){
-        modalNode.remove();
-    }
-}
+// const removeModal=()=>{
+//     if(undefined!==modalNode){
+//         modalNode.remove();
+//     }
+// }
 
 // function removeModale(){
 //     if(undefined!==modalNode){
@@ -29,19 +29,56 @@ const removeModal=()=>{
  * @param {string} titre titre du message
  * @param {HTMLElement|string} content contenu du message
  */
- const showModal=(titre,content)=>{
-    //const title = document.querySelector('#modal-title');
-    //title.innerHTML="Mon <u>nouveau</u> titre";
-    if(undefined===document.querySelector('#modal-title')){
-        removeModal();
-    }
+//  const showModal=(titre,content)=>{
+//     //const title = document.querySelector('#modal-title');
+//     //title.innerHTML="Mon <u>nouveau</u> titre";
+//     if(undefined!==document.querySelector('#modal-title')){
+//         removeModal();
+//     }
     
-    modalNode.querySelector('#modal-title').innerHTML=titre;
-    modalNode.querySelector('#modal-content').innerHTML=content;
+//     modalNode.querySelector('#modal-title').innerHTML=titre;
+//     modalNode.querySelector('#modal-content').innerHTML=content;
 
-    document.body.appendChild(modalNode);
+//     document.body.appendChild(modalNode);
+
+//  }
+
+ class Modal{
+
+    #refId
+    #modalNode
+    constructor(idModal="modal"){
+        this.#refId=idModal
+        document.addEventListener('DOMContentLoaded',(evt)=>{
+            this.#modalNode=document.querySelector(`#${this.#refId}`)
+            this.removeModal()
+        })
+    }
+   
+    /**
+     * fermeture du modal
+     */
+    removeModal=()=>{
+        if(undefined!==this.#modalNode){
+            this.#modalNode.remove()
+        }
+    }
+
+
+    showModal=(titre,content)=>{
+        if(null!==this.#modalNode){
+            this.removeModal()
+        }
+        
+        this.#modalNode.querySelector('#modal-title').innerHTML=titre
+        this.#modalNode.querySelector('#modal-content').innerHTML=content
+    
+        document.body.appendChild(this.#modalNode)
+    }
+        
 
  }
 
 
 
+const modal = new Modal('modal');
